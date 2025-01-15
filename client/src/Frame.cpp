05f8ec1975655f54e16837857c7dab4f9854ce4e
adicohen <adia5@post.bgu.ac.il>
2025-01-15 09:34:10 +0200

    frame implemantaion

--- client/src/Frame.cpp
+++ client/src/Frame.cpp
@@ -4,19 +4,63 @@
 #include <sstream>
 #include "../include/Frame.h"
 
-class Frame {
-private:
-    std::unordered_map<std::string, std::string> hashMap;
+Frame::Frame(const std::string &inputString)
+{
+    parseStringToHashMap(inputString);
+}
 
-public:
-    Frame(const std::string& inputString) {
-        parseStringToHashMap(inputString);
+void Frame::parseStringToHashMap(const std::string &inputString)
+{
+    std::istringstream stream(inputString);
+    std::string type;
+    std::string line;
+
+    // Extract the first line
+    if (std::getline(stream, type))
+    {
+        this->type = type;
     }
 
-    void parseStringToHashMap(const std::string& inputString) {
-    }
+    // Loop through the remaining lines
+    while (std::getline(stream, line))
+    {
+        if (!line.empty())
+        {
+            std::istringstream lineStream(line);
+            std::string key, value;
+            lineStream >> key;
+            std::getline(lineStream, value);
 
-    const std::string& getValue(const std::string& key) const {
-    }
+            // Trim leading space from value
+            if (!value.empty() && value[0] == ' ')
+            {
+                value.erase(0, 1);
+            }
+            data[key] = value;
+        }
+        else
+        {
+            std::string remainingContent;
+            data["body"]="";
+            while (std::getline(stream, remainingContent))
+            {
+                if (!remainingContent.empty())
+                {
+                    data["body"] += " " + remainingContent;
+                }
+            }
 
-};
+            break;
+        }
+    }
+}
+
+const std::string &Frame::getValue(const std::string &key) const
+{
+    return data.at(key);
+}
+
+const std::string &Frame::getType() const
+{
+    return type;
+}
