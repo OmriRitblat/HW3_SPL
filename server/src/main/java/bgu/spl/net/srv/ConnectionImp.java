83fc2d1f41c6e35cb6e172b5bbc11c6b28268a83
adicohen <adia5@post.bgu.ac.il>
2025-01-17 17:02:33 +0200

    implement connection class by iterface

--- server/src/main/java/bgu/spl/net/srv/ConnectionImp.java
+++ server/src/main/java/bgu/spl/net/srv/ConnectionImp.java
@@ -4,18 +4,26 @@ import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionImp<T> {
-    private ConcurrentHashMap<Integer, ConnectionHandler> clients;
-    private ConcurrentHashMap<String, List<Integer>> subsribtion;
+    private ConcurrentHashMap<Integer, ConnectionHandler> clients; //<connction id, connection handler>
+    private ConcurrentHashMap<String, List<Integer>> subsribtion; //<channel name, list<connection id>>
 
     public boolean send(int connectionId, T msg){
-        return true;
+        ConnectionHandler c=clients.get(connectionId)
+        if(c!=null)
+            return c.send(msg);
+        return false;
     }
 
     public void send(String channel, T msg){
-
+        List<Integer> sub=subsribtion.get(channel);
+        if(sub!=null)
+            for(Integer i:sub)
+                send(i,msg);
     }
 
     public void disconnect(int connectionId){
-
+        clients.remove(connectionId);
+        //should remove from subscibtion also?
     }
+    
 }
